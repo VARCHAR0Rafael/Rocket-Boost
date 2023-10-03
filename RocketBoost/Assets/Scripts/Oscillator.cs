fc70f704706e266b43b012dffc733b4b21e3ffa8
VARCHAR0Rafael <rtorresc@unah.hn>
2023-10-02 18:33:05 -0600

    New features
    -Created Obtacles that are able to move.
    -Added more levels.

--- RocketBoost/Assets/Scripts/Oscillator.cs
+++ RocketBoost/Assets/Scripts/Oscillator.cs
@@ -7,6 +7,7 @@ public class Oscillator : MonoBehaviour
     Vector3 startingPosition;
     [SerializeField] Vector3 movementVector;
     [SerializeField] [Range(0,1)] float movementFactor;
+    [SerializeField] float period = 2f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +17,12 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (period <= Mathf.Epsilon) { return; }
+        float cycles = Time.time / period; //continually growing 
+        const float tau = Mathf.PI * 2; //constant value of 6.283 
+        float rawSinWave = Mathf.Sin(cycles * tau); //moving from 1 to -1
+
+        movementFactor = (rawSinWave + 1f) / 2;
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPosition + offset;
     }
